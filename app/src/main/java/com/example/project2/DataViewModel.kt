6a3909ecab3f828345f7f3461da8930964960514
Arif Imeri <arifimeri.11@gmail.com>
2025-01-27 14:04:18 +0100

    first commit

---
+++ app/src/main/java/com/example/project2/DataViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.project2
+
+import androidx.lifecycle.ViewModel
+
+class DataViewModel : ViewModel() {
+    //logjika e api
+
+
+}
---
+++ app/src/main/java/com/example/project2/ui/NewFragment.kt
@@ -0,0 +1,26 @@
+package com.example.project2.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.project2.DataViewModel
+
+class NewFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        var viewModel = DataViewModel()
+//        viewModel.api //api call prej view model
+    }
+}
