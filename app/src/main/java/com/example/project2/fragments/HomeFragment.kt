c89b7af2bd214c8cea58419a3f3c197d41f3bc47
Arif Imeri <arifimeri.11@gmail.com>
2025-01-29 20:13:11 +0100

    new changes

--- app/src/main/java/com/example/project2/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/project2/fragments/HomeFragment.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.project2.
 
 class HomeFragment : Fragment() {
     private lateinit var binding: HomeFragmentBinding
